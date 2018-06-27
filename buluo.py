b3cb04cbc4cb93e27135b85a54f67889cc533b26
Administrator <moluo2008@gmail.com>
2018-06-27 19:05:42 +0800

    v0.0.1

--- buluo.py
+++ buluo.py
@@ -14,7 +14,6 @@ headers = {
     'Host': 'www.boolaw.com',
     'Upgrade-Insecure-Requests': '1',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36'
-
 }
 
 def get_page_index(page_number):
